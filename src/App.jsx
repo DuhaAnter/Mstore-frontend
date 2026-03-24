5d3532a92550d8ec8eb122f99e4f11cb84b5ba1c
Duha <duhaanter@gmail.com>
2026-03-24 20:05:19 +0200

    added TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App(){
 
   return(
+    <>
     <h1>M-Store Frontend is Running</h1>
+    <br />
+    <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+    </>
   )
 }
 
