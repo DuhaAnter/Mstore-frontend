3e7e35a477f8ec9b78c04e5fa7b90755701dcbab
Duha <duhaanter@gmail.com>
2026-03-24 20:42:26 +0200

    added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+
+ 
+
+
 function App(){
 
   return(
@@ -5,6 +11,8 @@ function App(){
     <h1>M-Store Frontend is Running</h1>
     <br />
     <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+    <br />
+    <Button>shadcn is running</Button>
     </>
   )
 }
