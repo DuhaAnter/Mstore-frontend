016c3cedc079d547c2abb0b06f69bd525044d224
Duha <duhaanter@gmail.com>
2026-06-09 17:58:46 +0300

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,48 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
+import { TfiSearch } from "react-icons/tfi";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
 
+//small reusable component
+function ActiveLink({ to, children }) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive ? "text-black font-medium" : "text-[#c3c3c3] hover:text-black"
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
 export default function Header() {
   return (
     <>
       <nav className=" py-3 px-10 flex justify-between items-center bg-white/80 backdrop-blur-md border-b border-slate-100 shadow-sm transition-all duration-300">
         <div className="logo ">
           <p className="font-semibold text-3xl  font-['Pinyon_Script']">
-            Fashions
+            M Store
           </p>
         </div>
-        <div className="menu flex justify-between w-1/2 text-[#7777779e]">
-            <Link to='/'>Home</Link>
-            <Link to='/p2'>page2</Link>
-            <Link to='/p3'>page3</Link>
-            <Link to='/p4'>page4</Link>
-            <Link to='/login'>Log In</Link>
-
-
+        <div className="menu flex justify-between w-1/2 text-[#c3c3c3] font-['Montserrat']">
+          <ActiveLink to="/">Home</ActiveLink>
+          <ActiveLink to="/p2">Men</ActiveLink>
+          <ActiveLink to="/p3">Women</ActiveLink>
+          <ActiveLink to="/p4">Kids</ActiveLink>
+          <ActiveLink to="/p5">Sale</ActiveLink>
+          <ActiveLink to="/p6">Categoreis</ActiveLink>
+          <div className="flex justify-evenly items-center  w-37 text-black text-xl">
+            <Link to="/p7">
+              <TfiSearch />
+            </Link>
+            <Link to="/p8">
+              <FaShoppingCart />
+            </Link>
+            <Link to="/profile">
+              <IoPerson />
+            </Link>
+          </div>
         </div>
       </nav>
     </>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,21 +1,28 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Profile() {
-const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  if (!user.userLoggedIn) {
+    navigate("/login");
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50 p-4">
       <h1 className="text-3xl md:text-5xl font-bold text-red-600 text-center mb-4">
         TO BE USER PROFILE PAGE ISA
       </h1>
-     {
-      user.userName? <h3 className="text-blue-400">hi {user.userName} 
-      <br />
-      yor are logged in 
-      </h3> : <h3>hi guest</h3>
-     } 
-     
+      {user.userName ? (
+        <h3 className="text-blue-400">
+          hi {user.userName}
+          <br />
+          yor are logged in
+        </h3>
+      ) : (
+        <h3>hi guest</h3>
+      )}
     </div>
   );
 }
