d0eb157420ef96a3c06e647a8d58d8ffd0ed3a6f
Duha <duhaanter@gmail.com>
2026-06-15 01:06:01 +0300

    changed api to test layout

--- src/products/api/products.js
+++ src/products/api/products.js
@@ -3,7 +3,7 @@ const API_URL = "http://localhost:5000/products/";
 import axios from "axios";
 
 export const allproducts = async () => {
-    const response = await axios.get(API_URL);
+    const response = await axios.get('https://dummyjson.com/products');
     //console.log(response.data);
     return response.data.products;
 }
--- src/products/components/ProductsGrid.jsx
+++ src/products/components/ProductsGrid.jsx
@@ -6,13 +6,13 @@ export default function ProductsGrid({ products }) {
     <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
       {products.map((product) => (
         <div className="card flex flex-col items-center rounded-lg shadow-md transition-all duration-300 hover:shadow-xl hover:-translate-y-1 ">
-          <ProductImage src={product.imageURL} />
+          <ProductImage src={product.images[0]} />
           <div className="p-3 text-center">
             <p className="font-semibold">{product.title}</p>
             <p className=" text-gray-500 text-center">
               {product.description.split(" ").slice(0, 4).join(" ") + "..."}
             </p>
-            <span className="font-semibold py-3">${product.minPrice}</span>
+            <span className="font-semibold py-3">${product.price}</span>
           </div>
         </div>
       ))}
