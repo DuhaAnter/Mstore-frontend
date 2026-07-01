bbf10c4f068cfae847200f53181cb8ef8b676842
Duha <duhaanter@gmail.com>
2026-07-01 14:03:27 +0300

    cart is alive

--- src/user/api/cart.js
+++ src/user/api/cart.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
- const API_URL = `${import.meta.env.VITE_API_URL}cart/`
+const API_URL = `${import.meta.env.VITE_API_URL}cart/`
 
 
-export const addToCart = async(variantId,quantity)=>{
-    const response = await axios.post(API_URL,{variantId,quantity});
+export const addToCart = async (variantId, quantity) => {
+    const response = await axios.post(API_URL, { variantId, quantity });
     return response.data;
-}; 
+};
+export const viewCart = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
+export const updateCartItem = async (id, quantity) => {
+    const response = await axios.patch(`${API_URL}${id}`, { quantity });
+    return response.data;
+};
+export const removeCartItem = async (id) => {
+    const response = await axios.delete(`${API_URL}${id}`);
+    return response.data;
+};
--- src/user/pages/Cart.jsx
+++ src/user/pages/Cart.jsx
@@ -1,75 +1,132 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OrderSummary from "../components/OrderSummary";
 import { FaTrash } from "react-icons/fa";
+import { removeCartItem, updateCartItem, viewCart } from "../api/cart";
 
 export default function Cart() {
-  //independent counts for independent products
-  const [count1, setCount1] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [variants, setVariants] = useState([]);
 
+  // calling api
+  useEffect(() => {
+    const fetchViewCart = async () => {
+      try {
+        const data = await viewCart();
+        // console.log("data of view cart -->", data.data);
+        setVariants(data.data);
+      } catch (error) {
+        console.log("error fetching veiw cart", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchViewCart();
+  }, [variants]);
+  //handlers
+  const handleItemQuantity = async (id, quantity) => {
+    try {
+      const update = await updateCartItem(id, quantity);
+      console.log(update);
+    } catch (error) {
+      console.log("error in handle item quantity", error);
+    }
+  };
+  const handleItemDelete = async (id) => {
+    try {
+      const remove = await removeCartItem(id);
+      console.log(remove);
+    } catch (error) {
+      console.log("error deleting item", error);
+    }
+  };
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center">
+        <p>loading cart</p>
+      </div>
+    );
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 p-4 md:p-10">
       <div className="cart lg:col-span-2 space-y-6">
         <h1 className="font-bold text-2xl md:text-3xl mb-5">My Cart</h1>
         <hr className="border-gray-200" />
 
-        <div className="prd flex items-center gap-4 mt-5 border-b border-gray-100 pb-6">
-          <div className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 shrink overflow-hidden rounded-xl">
-            <img
-              src="/imgs/photo_٢٠٢٦-٠٥-٠٧_٠٠-٥٨-٠٩.jpg"
-              className="w-full h-full object-cover"
-              alt="Pink Dress"
-            />
-          </div>
+        {variants?.map((v) => (
+          <div
+            key={v.id}
+            className="prd flex items-center gap-4 mt-5 border-b border-gray-100 pb-6"
+          >
+            <div className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 shrink overflow-hidden rounded-xl">
+              <img
+                src={v.variant.product.imageURL}
+                className="w-full h-full object-cover"
+                alt="Pink Dress"
+              />
+            </div>
 
-          <div className="flex-1 grid grid-cols-3">
-            <div className="col-span-2 flex flex-col gap-1">
-              <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-3">
-                <h1 className="font-semibold text-lg md:text-2xl truncate">
-                  Pink Dress
-                </h1>
-                <div className="flex items-center gap-1">
-                  <span className="text-lg text-[#ffc107]">★★★★★</span>
-                  <span className=" text-[8px] sm:text-xs text-gray-500">
-                    (3 Reviews)
-                  </span>
+            <div className="flex-1 grid grid-cols-3 ">
+              <div className="col-span-2 flex flex-col gap-1">
+                <div className="flex flex-col">
+                  <h1 className="font-semibold text-lg md:text-2xl truncate">
+                    {v.variant.product.title}
+                  </h1>
+                  <div className="flex items-center gap-1">
+                    <span className="text-lg text-[#ffc107]">★★★★★</span>
+                    <span className=" text-[8px] sm:text-xs text-gray-500">
+                      (3 Reviews)
+                    </span>
+                  </div>
+                </div>
+                <p className="text-gray-500 text-xs md:text-lg line-clamp-2 ">
+                  {v.variant.product.description}
+                </p>
+
+                <p className="text-xs md:text-sm">
+                  <strong>Size: </strong>
+                  {v.variant.size}
+                </p>
+                <p className="text-xs md:text-sm">
+                  <strong>Color: </strong>
+                  {v.variant.color}
+                </p>
+                <div className="flex items-center  gap-3">
+                  <div className="flex items-center bg-gray-100 rounded px-2.5 py-1 w-25">
+                    <button
+                      // onClick={() => setCount1(Math.max(1, v.quantity - 1))}
+                      onClick={() => handleItemQuantity(v.id, v.quantity - 1)}
+                      className="cursor-pointer text-lg font-bold text-gray-500 hover:text-black w-6 h-6 flex items-center justify-center transition"
+                    >
+                      {v.quantity === 1 ? <FaTrash /> : "−"}
+                    </button>
+                    <span className="mx-3 text-sm md:text-base font-semibold w-4 text-center select-none">
+                      {v.quantity}
+                    </span>
+                    <button
+                      onClick={() => handleItemQuantity(v.id, v.quantity + 1)}
+                      // onClick={() => setCount1(v.quantity + 1)}
+                      className="cursor-pointer text-lg font-bold text-gray-500 hover:text-black w-6 h-6 flex items-center justify-center transition"
+                    >
+                      +
+                    </button>
+                  </div>
+                  <button
+                    onClick={() => handleItemDelete(v.id)}
+                    className="pl-3 border-l border-gray-300 text-xs md:text-sm font-medium text-gray-500 hover:text-red-600 hover:underline transition-colors cursor-pointer"
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
-              <p className="text-gray-500 text-xs md:text-lg line-clamp-2 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-                deleniti quasi vero porro.
-              </p>
 
-              <p className="text-xs md:text-sm">
-                <strong>Size: </strong> small
-              </p>
-              <p className="text-xs md:text-sm">
-                <strong>Color: </strong> black
-              </p>
-
-              <div className="flex items-center bg-gray-100 rounded px-2.5 py-1 w-25">
-                <button
-                  onClick={() => setCount1(Math.max(1, count1 - 1))}
-                  className="cursor-pointer text-lg font-bold text-gray-500 hover:text-black w-6 h-6 flex items-center justify-center transition"
-                >
-                  {count1 === 1 ? <FaTrash /> : "−"}
-                </button>
-                <span className="mx-3 text-sm md:text-base font-semibold w-4 text-center select-none">
-                  {count1}
-                </span>
-                <button
-                  onClick={() => setCount1(count1 + 1)}
-                  className="cursor-pointer text-lg font-bold text-gray-500 hover:text-black w-6 h-6 flex items-center justify-center transition"
-                >
-                  +
-                </button>
+              <div className="flex justify-end items-start mt-2">
+                <p className="text-lg md:text-xl font-bold">
+                  $ {v.variant.price}
+                </p>
               </div>
             </div>
-
-            <div className="flex justify-end items-start mt-2">
-              <p className="text-lg md:text-xl font-bold">$100</p>
-            </div>
           </div>
-        </div>
+        ))}
       </div>
 
       <div className="order lg:col-span-1">
